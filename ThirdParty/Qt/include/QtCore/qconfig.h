1eb73d99326a1c23a23969436dad3b8ce5091a4d
kevinmore <dingfengyu@gmail.com>
2015-03-04 18:52:58 +0000

    Upgraded to qt5.4 x64

--- ThirdParty/Qt/include/QtCore/qconfig.h
+++ ThirdParty/Qt/include/QtCore/qconfig.h
@@ -6,12 +6,8 @@
 
 
 // Compiler sub-arch support
-#define QT_COMPILER_SUPPORTS_SSE2
-#define QT_COMPILER_SUPPORTS_SSE3
-#define QT_COMPILER_SUPPORTS_SSSE3
-#define QT_COMPILER_SUPPORTS_SSE4_1
-#define QT_COMPILER_SUPPORTS_SSE4_2
-#define QT_COMPILER_SUPPORTS_AVX
+#define QT_COMPILER_SUPPORTS_SSE2 1
+#define QT_COMPILER_SUPPORTS_SSE3 1
 
 // Compile time features
 #if defined(QT_LARGEFILE_SUPPORT) && defined(QT_NO_LARGEFILE_SUPPORT)
@@ -24,12 +20,6 @@
 # undef QT_NO_CUPS
 #elif !defined(QT_NO_CUPS)
 # define QT_NO_CUPS
-#endif
-
-#if defined(QT_NO_DBUS) && defined(QT_DBUS)
-# undef QT_NO_DBUS
-#elif !defined(QT_NO_DBUS)
-# define QT_NO_DBUS
 #endif
 
 #if defined(QT_NO_EVDEV) && defined(QT_EVDEV)
@@ -54,12 +44,6 @@
 # undef QT_NO_GLIB
 #elif !defined(QT_NO_GLIB)
 # define QT_NO_GLIB
-#endif
-
-#if defined(QT_NO_HARFBUZZ) && defined(QT_HARFBUZZ)
-# undef QT_NO_HARFBUZZ
-#elif !defined(QT_NO_HARFBUZZ)
-# define QT_NO_HARFBUZZ
 #endif
 
 #if defined(QT_NO_ICONV) && defined(QT_ICONV)
@@ -116,10 +100,16 @@
 # define QT_NO_STYLE_WINDOWSMOBILE
 #endif
 
+#if defined(QT_OPENGL_DYNAMIC) && defined(QT_NO_OPENGL_DYNAMIC)
+# undef QT_OPENGL_DYNAMIC
+#elif !defined(QT_OPENGL_DYNAMIC)
+# define QT_OPENGL_DYNAMIC
+#endif
+
 #if defined(QT_POINTER_SIZE) && defined(QT_NO_POINTER_SIZE)
 # undef QT_POINTER_SIZE
 #elif !defined(QT_POINTER_SIZE)
-# define QT_POINTER_SIZE 4
+# define QT_POINTER_SIZE 8
 #endif
 
 #define QT_QPA_DEFAULT_PLATFORM_NAME "windows"
