5e4499a645c2bdfc30dcc102763b40e480a111a6
kevinmore <dingfengyu@gmail.com>
2015-03-08 23:45:25 +0000

    Playing with CUDA

--- Source/main.cu
+++ Source/main.cu
@@ -1,8 +1,26 @@
-#include<iostream>
-#include <cuda.h>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include <cuda_runtime.h>
+
+__global__ void Calc(int a, int b, int *c)
+{
+	for (int i = 0; i < 9999999999; ++i)
+	{
+		*c = a + b;
+	}
+}
 
 int main()
 {
+	int a, b, c;
+	int *dev_c;
+	scanf("%d%d", &a, &b);
+	cudaMalloc(&dev_c, sizeof(int));
+	Calc<<<1, 1>>>(a, b, dev_c);
+	cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+	printf("%d\n", c);
+	system("pause");
 	return 0;
+
 }
